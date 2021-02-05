b213885f0689e57e3e371209c9c6eafa9b3781a1
Clayton R. Farias <47605415+claytonfariasEC@users.noreply.github.com>
2021-02-05 17:54:59 -0300

    Add files via upload

--- raspagem de dados leilão.py
+++ raspagem de dados leilão.py
@@ -31,7 +31,7 @@ class Webscraper:
 		print("CURRENT PAGE URL IS:", self.url)
 
 		#all_anuncios = soup.find_all("div", class_="visualizacaoDiv-titulo")
-		resultado = []
+
 
 		#for x in soup.find_all("li", class_="visualizacaoDiv-border act-ss-filtro-lista-img lt-jucicial-1 act-classe-lote0"):
 		#i = 0
@@ -40,29 +40,47 @@ class Webscraper:
 		dict_anuncios = {}
 		url_base = "https://www.sodresantoro.com.br/"
 
-		for i in range (15):
+		lista_anuncio = []
+		lista_url = []
 
+		for i in range (15):
+			#visualizacaoDiv - lance
+			#tipo - V
 			for anuncio in soup.find_all(attrs={"class": "visualizacaoDiv-border act-ss-filtro-lista-img lt-jucicial-1 act-classe-lote"+str(i)}):
 
 				print("-------------------------------------")
 				#print(anuncio.div)
 				link = anuncio.div
-
 				#print(link)
-
 				for x in link.find_all(attrs={"class": "visualizacaoDiv-titulo-lote"}):
 						#print(x)
 						#print(x.prettify())
 						#print("---------")
+
 						for zz in link.find_all(attrs={"class": "titulo_1"}):
 							#print(zz)
-							dict_anuncios[i] = {"anuncio":zz.get_text() , "url": url_base + x.get("href")}
 
-						#print(x.find("titulo_1"))
-						#print("-> "  + x.get("href"))
-				#print(anuncio)
-				#y = anuncio.div
-				#x = y.find('a')
+							dict_anuncios[i] = {"anuncio": zz.get_text() , "url": url_base + x.get("href"), "preco": ""}
+		index = 0
+		for y in soup.find_all(attrs={"class": "lance"}):
+							print(index)
+							#print("->"+ str(y.get_text()))
+							if(index >= 15):
+								dict_anuncios[index-15] = {"anuncio": dict_anuncios[index-15]['anuncio'] , "url": dict_anuncios[index-15]['url'] , "preco": str(y.get_text())}
+							index = index + 1
+							#print(">>>> "+ str(y.get_text()))
+							#list_price.append(y.get_text())
+							#dict_anuncios[index] =  {"anuncio": dict_anuncios[index]["anuncio"], "url": dict_anuncios[index]["url"], "preco": y.get_text()}
+							#index = index + 1
+							#for price link.find_all(attr={"class" : "visualizacaoDiv-lance tipo-V"}):
+
+							#for i in range(15):
+							#		dict_anuncios[i] = {"anuncio": list_anuncio[i], "url": list_price[i], "preco": ""}
+								#print(x.find("titulo_1"))
+								#print("-> "  + x.get("href"))
+						#print(anuncio)
+						#y = anuncio.div
+						#x = y.find('a')
 
 
 				#print(type(x))
@@ -70,7 +88,7 @@ class Webscraper:
 				#z = x.find('class="visualizacaoDiv-titulo-lote"')
 				#print("XXXXXXXXXXXXX: " + str(x))
 				#i = i + 1
-				print("-----------------" +str(i) + "--------------------\n\n")
+		print("-----------------" +str(i) + "--------------------\n\n")
 
 		#print(">>>" + str(lista_anuncios))
 		#print(dict_anuncios)
@@ -78,7 +96,7 @@ class Webscraper:
 			print("-------------------------------------------------")
 			print(str(i)  +  " - " + str(dict_anuncios[i]))
 			raspagem = Webscraper(dict_anuncios[i]['url'])
-			raspagem.get_price()
+			#raspagem.get_price()
 			print("-------------------------------------------------")
 		#print(soup.prettify())
 
